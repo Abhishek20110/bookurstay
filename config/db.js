cf455a2e36cc6efbd1b4b308677495676d56cf68
Abhishek20110 <71166991+Abhishek20110@users.noreply.github.com>
2024-09-21 14:09:43 +0530

    last

--- config/db.js
+++ config/db.js
@@ -1,21 +1,32 @@
 import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
 
+// Load environment variables
 dotenv.config();
 
+// Create a new Sequelize instance
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
 });
 
+// Function to authenticate the connection
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('Database connection established successfully.');
     } catch (error) {
-        console.error('Unable to connect to the database:', error.message);
+        console.error('Unable to connect to the database:', error);
+        console.error(error.stack);
         process.exit(1); // Exit on failure
     }
 };
 
+// Export the sequelize instance and connectDB function
 export { sequelize, connectDB };
