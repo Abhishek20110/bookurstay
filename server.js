a9ff6c02c4718dd0429013266675cd823008f64e
Abhishek20110 <71166991+Abhishek20110@users.noreply.github.com>
2024-09-21 13:10:12 +0530

    simple

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import { connectDB } from './config/db.js';
-import faqRoutes from './routes/faqRoutes.js'; // Import the FAQ routes
 
 dotenv.config(); // Load environment variables
 const app = express();
@@ -12,9 +11,6 @@ app.use(express.json());
 
 // Connect to the database
 connectDB();
-
-// Use the FAQ routes for all /api/faqs requests
-app.use('/api/faqs', faqRoutes);
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
