5d949d47f55c1716780221ad980ab84b234a78e4
Abhishek20110 <71166991+Abhishek20110@users.noreply.github.com>
2024-09-21 14:31:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { Sequelize } from 'sequelize';
+//import { connectDB } from './config/db.js';
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Create a new Sequelize instance for database connection
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql', // Use 'mysql' here
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-});
-
-// Function to authenticate the connection
-const connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Database connection established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-        process.exit(1); // Exit the application on error
-    }
-};
-
-// Connect to the database
-connectDB();
+// Connect to the database gvv
+//connectDB();
 
 // Middleware to parse JSON requests
 app.use(express.json());
