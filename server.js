df50b5e0e2650863ff97190d844d1295e0e5ed30
Abhishek20110 <71166991+Abhishek20110@users.noreply.github.com>
2024-09-21 15:01:31 +0530

    kk

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { connectDB } from './db.js'; // Ensure this path is correct
+import { connectDB } from './config/db.js'; // Ensure this path is correct
 
 dotenv.config();
 
