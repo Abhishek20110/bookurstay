8dbf187d7fc29b5de15fa32cc65918478522db39
Abhishek20110 <71166991+Abhishek20110@users.noreply.github.com>
2024-09-21 14:53:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql', // Use 'mysql'
+     dialect: 'mysql',
+    dialectModule: require('mysql2')l'
     pool: {
         max: 5,
         min: 0,
